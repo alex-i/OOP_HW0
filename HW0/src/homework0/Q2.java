30e083dc12d2738aa1cdf8051060a042643eeb94
Alex Isenshtat <alexis@t2.technion.ac.il>
2014-11-13 17:02:15 +0200

    hi :)

--- HW0/src/homework0/Q2.java
+++ HW0/src/homework0/Q2.java
@@ -1,5 +1,26 @@
 package homework0;
-//bla
-public class Q2 {
 
+import java.io.*;
+
+public class Q2 {
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1)
+		{
+			System.out.println("Invalid arguments");
+			return;
+		}
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(args[0]));
+		} catch (Exception e) {
+			System.out.println("File doesn't exists");
+			return;
+		}
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			System.out.println(line);
+		}
+		reader.close();
+		
+	}
 }
